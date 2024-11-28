d2b5b5d187830128d01673a281729b1454e779cd
Moses <45958030+MosesAweda@users.noreply.github.com>
2024-11-28 14:58:04 +0100

    Meta Issues fix

--- src/components/ViewCampaign/ViewCampaign.tsx
+++ src/components/ViewCampaign/ViewCampaign.tsx
@@ -217,7 +217,7 @@ const ViewCampaign = ({ item }: any) => {
         />
         <meta
           property="og:image"
-          content={campaignData?.campaignFiles?.[0]?.filePath || "default-image.png"}
+          content={campaignData?.campaignFiles?.[0]?.filePath || "https://res.cloudinary.com/dgso4wgqt/image/upload/v1732801029/logo_zganue.png"}
         />
         <meta
           property="og:url"
